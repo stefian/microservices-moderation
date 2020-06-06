6bcac4f39fad7954badd1dd1c224bee5bc04b105
stefian <stefan.ianta@w3ai.net>
2020-06-06 05:53:52 -0300

    added listening on 4003 and start script

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ const axios = require(axios);
 const app = express();
 app.use(bodyParser());
 
+app.post('/events', (req, res) => {
+
+});
+
+app.listen(4003, () => {
+  console.log('Moderation: Listening on 4003');
+});
