98db5bcb2b8c50dc31b591629c35758dd00ffc88
stefian <stefan.ianta@w3ai.net>
2020-06-06 09:05:18 -0300

    added comment status logic ? rejected : approved and built the axios event message including status

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentModerated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content
+      }
+    });
+  }
 
 });
 
